4d1526fbb7fa953b1cec7a88a77f7b4abfbf646b
Chris Ware <chris.ware@hooq.tv>
2018-03-21 22:12:35 +0800

    Handle multiple addresses in on API request

--- README.md
+++ README.md
@@ -4,25 +4,47 @@ Companion APIs to Watchmaker "24hr Worlder 6 Zone" watchface
 
 # API
 
-Get the timeszones and offset for a list of addresses. Uses Google Geocode and Timezone APIs.
+Get the timeszones and offsets for a list of addresses. Uses Google Geocode and Timezone APIs.
 
 ```
-/api/v1/timezone?address=india,singapore&key=<GOOGLE_API_KEY>
+/api/v1/timezone?address=india&address=singapore&address=UK&key=<GOOGLE_API_KEY>
 ```
 
 Returns
 
 ```javascript
-{
-  dstOffset: 0,
-  rawOffset: 28800,
-  status: "OK",
-  timeZoneId: "Asia/Singapore",
-  timeZoneName: "Singapore Standard Time",
-  abbr: "+08",
-  longName: "3",
-  shortName: "3"
-}
+[
+  {
+    dstOffset: 0,
+    rawOffset: 19800,
+    status: "OK",
+    timeZoneId: "Asia/Calcutta",
+    timeZoneName: "India Standard Time",
+    abbr: "IST",
+    longName: "India",
+    shortName: "IN"
+  },
+  {
+    dstOffset: 0,
+    rawOffset: 28800,
+    status: "OK",
+    timeZoneId: "Asia/Singapore",
+    timeZoneName: "Singapore Standard Time",
+    abbr: "+08",
+    longName: "Singapore",
+    shortName: "SG"
+  },
+  {
+    dstOffset: 0,
+    rawOffset: 0,
+    status: "OK",
+    timeZoneId: "Europe/London",
+    timeZoneName: "Greenwich Mean Time",
+    abbr: "GMT",
+    longName: "United Kingdom",
+    shortName: "GB"
+  }
+]
 ```
 
 # Development
--- timezone.js
+++ timezone.js
@@ -3,42 +3,53 @@
 const moment = require('moment-timezone');
 const deepExtend = require('deep-extend');
 
-// /api/v1/timezone?address=singapore&key=<YOURAPIKEY>
+// /api/v1/timezone?address=singapore&address=india&key=<YOURAPIKEY>
 module.exports.get = (event, context, callback) => {
   const qs = event.queryStringParameters;
   const addresses = [].concat(qs.address); //convert one address into array
 
   const googleMaps = require('./googlemaps').with(qs.key);
 
-  googleMaps.geocode({
-      address: addresses[0]//'Sydney Opera House'
-    })
-    .asPromise()
-    .then((result) => {
+  // map all the timezone objects for each address into an array
+  let timezones = addresses.map(addr => {
+    return new Promise((resolve,reject) => {
 
-      const loc = result.json.results.length>0 && result.json.results[0].geometry.location || {lat:0,lng:0}
-      const longName = result.json.results[0].address_components[0].long_name;
-      const shortName = result.json.results[0].address_components[0].short_name;
-      googleMaps.timezone({
-          location: [loc.lat, loc.lng],
-          language: 'en'
-        }).asPromise()
-        .then((result) => {
-          //console.log(result.json)
-          const abbr = moment.tz.zone(result.json.timeZoneId).abbr(moment()); // PDT
+      // make a request to Google geocode to get the lat/long for the address
+      return googleMaps.geocode({
+        address:addr
+      })
+      .asPromise()
+      .then((result) => {
+        const loc = result.json.results.length>0 && result.json.results[0].geometry.location || {lat:0,lng:0}
+        const longName = result.json.results[0].address_components[0].long_name;
+        const shortName = result.json.results[0].address_components[0].short_name;
 
-          const response = {
-            statusCode: 200,
-            body: JSON.stringify(deepExtend(result.json,{abbr:abbr,longName:longName,shortName:shortName})),
-          };
-          callback(null, response);
-        })
-        .catch((err) => {
-          callback(err, "ERROR getting timezone from lat/lng");
-        });
+        // make a request to Google timezone to get the timezone for the lat/long
+        googleMaps.timezone({
+            location: [loc.lat, loc.lng],
+            language: 'en'
+          })
+          .asPromise()
+          .then(result => {
+
+            // use moment.js to lookup the timezone abbreviation e.g. 'PDT'
+            const abbr = moment.tz.zone(result.json.timeZoneId).abbr(moment());
+
+            // extend the Google result with the address name and timezone abbreviation
+            resolve(deepExtend(result.json,{abbr:abbr,longName:longName,shortName:shortName}));
+          })
+      })
     })
-    .catch((err) => {
-      console.log(err)
-      callback(err, "ERROR getting lat/lng from address " + qs);
-    });
+  })
+
+  // for all the timezone objects
+  Promise.all(timezones)
+  .then(data => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+    callback(null, response);
+
+  })
 };
